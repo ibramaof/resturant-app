071ff4238ba9371ed3e1ab354e8b171da830f8c7
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2026-01-02 16:21:42 +0200

    rendered all items in default state

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ import { menuArray } from "./data.js";
 const menuItems = document.getElementById('default-state')
 
 
-// function render() {
-//     menuItems.innerHTML = ''
-// }
-// render()
+
+
+
+function getItems() {
+    return menuArray.map(({ name, ingredients, price, id, emoji }) => {
+
+        return `
+    <div class="item">
+                <p class="item-emoji">${emoji}</p>
+                <div class="item-info">
+                    <p class="item-name">${name}</p>
+                    <P class="item-ingredients">${ingredients}</P>
+                    <p class="item-price">${price}$</p>
+                </div>
+                <button class="add-btn"><span>+</span></button>
+            </div>
+    `
+    }).join('')
+}
+
+
+function render() {
+    menuItems.innerHTML = getItems()
+}
+render()
 
